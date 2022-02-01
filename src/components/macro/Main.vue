ec5777eb0d69d8aa46ea308612ddd6bdceadad5b
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-01 03:47:37 +0100

    Creato main

---
+++ src/components/macro/Main.vue
@@ -0,0 +1,26 @@
+<template>
+<main>
+    <div class="container">
+        <Albums />
+    </div>
+</main>
+</template>
+
+<script>
+import Albums from "../sections/Albums.vue"
+export default {
+    name: "Main",
+    components: {
+        Albums
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/style/vars.scss";
+
+main {
+    height: calc(100vh - 48px);
+    background-color: $bg-color;
+}
+</style>
