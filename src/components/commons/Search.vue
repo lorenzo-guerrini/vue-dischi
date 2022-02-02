80eb754fa77a4ebd0d2c25add6a206d865e2996f
lorenzo-guerrini <lorenzoguerrini8@live.it>
2022-02-02 10:54:43 +0100

    Ricerca tramite genere, artista e titolo

---
+++ src/components/commons/Search.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container">
+      <div>
+          <input type="text" v-model="inputText" placeholder="Cerca per titolo" @keyup="$emit('searchTitle', inputText)">
+          <button @click="$emit('search', inputText)">Cerca</button>
+      </div>
+      <div>
+          <span>Filter by genre: </span>
+          <select v-model="inputGenre" @change="$emit('filterGenre', inputGenre)">
+            <option v-for="(genre, i) in genres" :key="i">{{genre}}</option>
+         </select>
+      </div>
+      <div>
+          <span>Filter by artist: </span>
+          <select v-model="inputArtist" @change="$emit('filterArtist', inputArtist)">
+              <option v-for="(artist, i) in artists" :key="i">{{artist}}</option>
+         </select>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "Search",
+    data() {
+        return {
+            genres: [
+                "all",
+                "rock",
+                "pop",
+                "jazz",
+                "metal"
+            ],
+            artists: [
+                "all",
+                "bon jovi",
+                "queen",
+                "sting",
+                "steve gaddband",
+                "iron maiden",
+                "eric clapton",
+                "deep purple",
+                "metallica",
+                "david wecki",
+                "michael jacjson",
+            ],
+            inputText: "",
+            inputGenre: "all",
+            inputArtist: "all",
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+    color: white;
+}
+</style>
--- src/components/sections/Albums.vue
+++ src/components/sections/Albums.vue
@@ -1,16 +1,18 @@
 <template>
-  <div>
-    <div v-if="!loading" class="container">
-      <Album v-for="(element, i) in albumsArray" :key="i" :info="element" />
+  <div class="container">
+    <Search @searchTitle="searchTitle" @filterGenre="filterGenre" @filterArtist="filterArtist"/>
+    <div v-if="!loading" class="albums-container">
+      <Album v-for="(element, i) in searchedAlbums" :key="i" :info="element" />
     </div>
     <Loader v-else />
   </div>
 </template>
 
 <script>
-import Loader from "../commons/Loader.vue";
+import Search from "../commons/Search.vue";
 import axios from "axios";
 import Album from "../commons/Album.vue";
+import Loader from "../commons/Loader.vue";
 
 export default {
   name: "Albums",
@@ -19,9 +21,13 @@ export default {
       apiURL: "https://flynn.boolean.careers/exercises/api/array/music",
       albumsArray: [],
       loading: true,
+      inputSearch: "",
+      inputGenre: "all",
+      inputArtist: "all",
     };
   },
   components: {
+    Search,
     Loader,
     Album,
   },
@@ -37,15 +43,52 @@ export default {
           console.log(error);
         });
     },
+    searchTitle(input) {
+      this.inputSearch = input;
+    },
+    filterGenre(input) {
+      this.inputGenre = input;
+    },
+    filterArtist(input) {
+      this.inputArtist = input;
+    },
   },
   created() {
     this.getAlbums();
   },
+  computed: {
+    searchedAlbums() {
+      return this.albumsArray.filter((album) => {
+        if (this.inputGenre == "all" && this.inputArtist == "all") {
+          return album.title.toLowerCase().includes(this.inputSearch.toLowerCase());
+        }
+
+        if(this.inputGenre != "all" && this.inputArtist == "all") {
+          return album.title.toLowerCase().includes(this.inputSearch.toLowerCase()) &&
+          album.genre.toLowerCase().includes(this.inputGenre.toLowerCase());
+        }
+
+        if(this.inputGenre == "all" && this.inputArtist != "all") {
+          return album.title.toLowerCase().includes(this.inputSearch.toLowerCase()) &&
+          album.author.toLowerCase().includes(this.inputArtist.toLowerCase());
+        }
+
+        return album.title.toLowerCase().includes(this.inputSearch.toLowerCase()) &&
+          album.genre.toLowerCase().includes(this.inputGenre.toLowerCase()) &&
+          album.author.toLowerCase().includes(this.inputArtist.toLowerCase());
+      })
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
 .container {
+  text-align: center;
+  padding-top: 40px;
+}
+
+.albums-container {
   width: 900px;
 
   margin: auto;
